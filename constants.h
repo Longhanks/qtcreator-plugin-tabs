e8e87174d2daf836fcab2964509960c518caa055
Andreas Schulz <andi.schulz@me.com>
2019-05-30 00:28:00 +0200

    Remove unused constants

--- constants.h
+++ constants.h
@@ -2,8 +2,6 @@
 
 namespace Tabs::Constants {
 
-const char ACTION_ID[] = "TabsPlugin.Action";
-const char MENU_ID[] = "TabsPlugin.Menu";
 const char PREV_TAB_ID[] = "TabsPlugin.PrevTab";
 const char NEXT_TAB_ID[] = "TabsPlugin.NextTab";
 
